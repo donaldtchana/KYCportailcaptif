35ec90e764b9e46e82baf07654fe5b4a19a0dca3
WHITE <raoulwht@gmail.com>
2023-02-24 17:14:42 +0100

    icon dashboard

--- admin/views/partial/sidebar.php
+++ admin/views/partial/sidebar.php
@@ -3,33 +3,33 @@
         <nav class="sidebar-nav">
             <ul id="sidebarnav">
                 <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
-                                             href="index" aria-expanded="false"><i class="mdi me-2 mdi-gauge"></i><span
+                                             href="index" aria-expanded="false"><i class="ti-panel"></i><span
                                 class="hide-menu">Tableau de bord</span></a></li>
                 <?php
                 if(isset($_SESSION['email_admin'])){
                 ?>
                 <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
                                              href="boutiques" aria-expanded="false">
-                        <i class="mdi me-2 mdi-account-check"></i><span class="hide-menu">Boutiques</span></a>
+                        <i class="ti-shopping-cart"></i><span class="hide-menu">Boutiques</span></a>
                 </li>
                 <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
-                                             href="cat_boutiques" aria-expanded="false"><i class="mdi me-2 mdi-table"></i><span
+                                             href="cat_boutiques" aria-expanded="false"><i class="ti-shopping-cart-full"></i><span
                                 class="hide-menu">Catégories boutiques</span></a></li>
                 <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
-                                             href="cat_articles" aria-expanded="false"><i class="mdi me-2 mdi-table"></i><span
+                                             href="cat_articles" aria-expanded="false"><i class="ti-package"></i><span
                                 class="hide-menu">Catégories d´articles</span></a></li>
                     <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
-                                                 href="offres" aria-expanded="false"><i class="mdi me-2 mdi-table"></i><span
+                                                 href="offres" aria-expanded="false"><i class="ti-gift"></i><span
                                     class="hide-menu">Offres</span></a></li>
                 <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
-                                             href="clients" aria-expanded="false"><i class="mdi me-2 mdi-table"></i><span
+                                             href="clients" aria-expanded="false"><i class="ti-id-badge"></i><span
                                 class="hide-menu">Clients</span></a></li>
                 <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
                                              href="logs" aria-expanded="false">
-                        <i class="mdi me-2 mdi-account-check"></i><span class="hide-menu">Logs</span></a>
+                        <i class="ti-bookmark-alt"></i><span class="hide-menu">Logs</span></a>
                 <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
                                              href="parametres" aria-expanded="false">
-                        <i class="mdi me-2 mdi-account-check"></i><span class="hide-menu">Paramètres</span></a>
+                        <i class="ti-settings"></i><span class="hide-menu">Paramètres</span></a>
                     <?php } else  {?>
 
                     <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
@@ -41,7 +41,7 @@
                     <?php } ?>
 
                 <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
-                                             href="logout" aria-expanded="false"><i class="mdi me-2 mdi-table"></i><span
+                                             href="logout" aria-expanded="false"><i class="ti-power-off"></i><span
                                 class="hide-menu">Deconnexion</span></a></li>
             </ul>
 
